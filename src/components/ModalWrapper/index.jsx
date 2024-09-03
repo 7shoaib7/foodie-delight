aed9dd73305926ad40e731374d1913fd30e5a1eb
7shoaib7 <69447663+7shoaib7@users.noreply.github.com>
2024-09-03 20:58:20 +0530

    modal-modified

--- src/components/ModalWrapper/index.jsx
+++ src/components/ModalWrapper/index.jsx
@@ -1,25 +1,16 @@
 import React from 'react';
 import { Modal, Box } from '@mui/material';
+import "./index.css";
 
 const ModalWrapper = ({ open, handleClose, children }) => {
   return (
     <Modal open={open} onClose={handleClose}>
-      <Box sx={{ ...modalStyle }}>
+      <Box className="modal-box">
         {children}
       </Box>
     </Modal>
   );
 };
 
-const modalStyle = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  boxShadow: 24,
-  p: 4,
-};
 
 export default ModalWrapper;
